a265e5b21775920d72b5fa0d63a8e9ecdb54cadf
Hatter Jiang <jht5945@gmail.com>
2019-07-20 23:45:55 +0800

    first version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # makepassword
+
+
+```
+$ makepassword -h
+Usage:
+  makepassword [OPTIONS]
+
+makepassword - command line makepassword tool.
+
+Optional arguments:
+  -h,--help             Show this help message and exit
+  -c,--count COUNT      Password count, default 1
+  -l,--length LENGTH    Password length, default 12
+  --chars CHARS         Chars, default use -t/--type base58
+  -t,--type TYPE        Type, base58(default), alphabet, word, all
+  -v,--version          Print version
+```
+
+
---
+++ src/main.rs
@@ -0,0 +1,104 @@
+extern crate argparse;
+extern crate rand;
+extern crate term;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
+const VERSION: &str = "0.1";
+const CHARS_DIGITALS: &str = "1234567890";
+const CHARS_LOWER_CASE: &str = "abcdefghijklmnopqrstuvwxyz";
+const CHARS_UPPER_CASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const CHARS_BASE58: &str = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+const CHARS_SYMBOL: &str = "-_.|!@#$%^&*()+=[]{};:<>";
+
+fn print_version() {
+    print!(r#"makepassword {}
+
+Copyright (C) 2019 Hatter Jiang.
+License MIT <https://opensource.org/licenses/MIT>
+Written by Hatter Jiang
+"#, VERSION);
+}
+
+pub enum MessageType { INFO, OK, WARN, ERROR, }
+
+pub fn print_message_ex(color: Option<term::color::Color>, h: &str, message: &str) {
+    let mut t = term::stdout().unwrap();
+    match color {
+        Some(c) => t.fg(c).unwrap(),
+        None => (),
+    }
+    write!(t, "{}", h).unwrap();
+    t.reset().unwrap();
+    println!(" {}", message);
+}
+
+pub fn print_message(mt: MessageType, message: &str) {
+    match mt {
+        MessageType::OK => print_message_ex(Some(term::color::GREEN), "[OK   ]", message),
+        MessageType::WARN => print_message_ex(Some(term::color::YELLOW), "[WARN ]", message),
+        MessageType::ERROR => print_message_ex(Some(term::color::RED), "[ERROR]", message),
+        MessageType::INFO => print_message_ex(None, "[INFO]", message),
+    }
+}
+
+fn main() {
+    let mut version = false;
+    let mut chars = String::new();
+    let mut chars_type = String::from("base58");
+    let mut password_count = 1u32;
+    let mut password_length = 12u8;
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("makepassword - command line makepassword tool.");
+        ap.refer(&mut password_count).add_option(&["-c", "--count"], Store, "Password count, default 1");
+        ap.refer(&mut password_length).add_option(&["-l", "--length"], Store, "Password length, default 12");
+        ap.refer(&mut chars).add_option(&["--chars"], Store, "Chars, default use -t/--type base58");
+        ap.refer(&mut chars_type).add_option(&["-t", "--type"], Store, "Type, base58(default), alphabet, word, all");
+        ap.refer(&mut version).add_option(&["-v", "--version"], StoreTrue, "Print version");
+        ap.parse_args_or_exit();
+    }
+    
+    if version {
+        print_version();
+        return;
+    }
+
+    if password_count < 1 || password_count > 100 {
+        print_message(MessageType::ERROR, &format!("Invalid count: {}", password_count));
+        return;
+    }
+
+    if password_length < 1 || password_length > 100 {
+        print_message(MessageType::ERROR, &format!("Invalid length: {}", password_length));
+        return;
+    }
+
+    if chars.len() > 0 && chars.len() < 8 {
+        print_message(MessageType::ERROR, &format!("Chars too small: {}", &chars));
+        return;
+    }
+
+    let chars_source: &str = & match chars.len() {
+        0 => match chars_type.as_str() {
+            "base58" => String::from(CHARS_BASE58),
+            "alphabet" => [CHARS_LOWER_CASE, CHARS_UPPER_CASE].join(""),
+            "word" => [CHARS_DIGITALS, CHARS_LOWER_CASE, CHARS_UPPER_CASE].join(""),
+            "all" => [CHARS_DIGITALS, CHARS_LOWER_CASE, CHARS_UPPER_CASE, CHARS_SYMBOL].join(""),
+            _ => {
+                print_message(MessageType::ERROR, &format!("Unknown type: {}", chars_type));
+                return;
+            },
+        },
+        _ => chars,
+    };
+
+    for _ in 0..password_count {
+        let mut password = String::new();
+        for _ in 0..password_length {
+            let p = rand::random::<usize>() % chars_source.len();
+            password.push_str(&chars_source[p..p+1]);
+        }
+        println!("{}", password);
+    }
+}
